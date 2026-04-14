f3af8a1d952a9e19a0bdf464ca4d4625d207c0d0
vinay <vinay.samtani@tryangled.com>
2026-04-14 14:01:37 -0700

    Add Mailwing 1.1.3 winget manifest

--- distribution/homebrew/mailwing.rb
+++ distribution/homebrew/mailwing.rb
@@ -1,6 +1,6 @@
 cask "mailwing" do
-  version "1.1.0"
-  sha256 "931806ffb8968f2af84afefa12605fd3a40aec60d75cd0864a8ad59c8417b030"
+  version "1.1.3"
+  sha256 "61367a5278c4c220b213178b9c5b2fe14b2f620ca0717f755ba8ffe87186ee1a"
 
   url "https://github.com/vinaysamtani/mailwing/releases/download/v#{version}/Mailwing-#{version}-universal.dmg"
   name "Mailwing"
